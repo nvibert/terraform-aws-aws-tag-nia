99626f9e425c7f7719d3020e6c36345a197a87b2
Nico Vibert <nico.vibert@hashicorp.com>
2021-10-15 16:46:03 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,5 @@
-#
-# var.services is a required input variable for Consul Terraform Sync
-#
-# An example of the services input value:
-# services = {
-#   "app-id-01.worker-01.dc1" : {
-#     id              = "app-id-01"
-#     name            = "app"
-#     ...
-#   },
-#   "web.worker-01.dc1" : {
-#     ...
-#   }
-# }
-#
 variable "services" {
-  description = "Consul services monitored by Consul-Terraform-Sync"
+  description = "Consul services monitored by Consul Terraform Sync"
   type = map(
     object({
       id        = string
@@ -39,27 +24,13 @@ variable "services" {
   )
 }
 
-#
-# Your module for can include input variables to be used by CTS operators for
-# customizing the module based on their infastructure. CTS supports both
-# optional and required variables.
-#
-variable "address_group_prefix" {
-  description = "Prefix added to each address group name"
-  type        = string
+variable "consul_kv" {
+  description = "Keys and values of the Consul KV pairs monitored by Consul-Terraform-Sync"
+  type        = map(string)
 
-  # Set the default argument to a default value to declare an optional variable.
-  # Omit the default argument for required ariables.
-  default = ""
-
-  # Set the sensitive flag for input variables that contain secret or sensitive
-  # values. When set, Terraform will redact the value from output when Terraform
-  # commands are run.
-  sensitive = false
+  default = {
+    tag_key = "tag_value"
+  }
 }
 
-variable "address_group_tags" {
-  description = "List of tag names to add to each address group for filtering of Consul service IPs"
-  type        = list(string)
-  default     = []
-}
+
